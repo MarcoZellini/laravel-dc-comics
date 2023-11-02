4c71e4355102126b1f039d5812d2fa86123f1879
Marco Zellini <zellinimarco@gmail.com>
2023-11-02 18:35:50 +0100

    Add edit

--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -10,12 +10,13 @@
             <div class="mb-3">
                 <label for="title" class="form-label">Title *</label>
                 <input type="text" class="form-control" name="title" id="title" aria-describedby="helpTitle"
-                    placeholder="Insert a comic title">
+                    placeholder="Insert a comic title" value="{{ $comic->title }}">
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
                 <input type="text" class="form-control" name="description" id="description"
-                    aria-describedby="helpdescription" placeholder="Insert a comic description">
+                    aria-describedby="helpdescription" placeholder="Insert a comic description"
+                    value="{{ $comic->description }}">
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Choose file</label>
@@ -25,32 +26,32 @@
             <div class="mb-3">
                 <label for="price" class="form-label">Price *</label>
                 <input type="text" class="form-control" name="price" id="price" aria-describedby="helpprice"
-                    placeholder="Insert a comic price">
+                    placeholder="Insert a comic price" value="{{ $comic->price }}">
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
                 <input type="text" class="form-control" name="series" id="series" aria-describedby="helpseries"
-                    placeholder="Insert a comic series">
+                    placeholder="Insert a comic series" value="{{ $comic->series }}">
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Sale Date</label>
                 <input type="date" class="form-control" name="sale_date" id="sale_date" aria-describedby="helpsale_date"
-                    placeholder="Insert a comic sale_date">
+                    placeholder="Insert a comic sale_date" value="{{ $comic->sale_date }}">
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
                 <input type="text" class="form-control" name="type" id="type" aria-describedby="helptype"
-                    placeholder="Insert a comic type">
+                    placeholder="Insert a comic type" value="{{ $comic->type }}">
             </div>
             <div class="mb-3">
                 <label for="artists" class="form-label">Artists (Separare i valori tra le virgole)</label>
                 <input type="text" class="form-control" name="artists" id="artists" aria-describedby="helpartists"
-                    placeholder="Insert a comic artists">
+                    placeholder="Insert a comic artists" value="{{ implode(', ', $comic->artists) }}">
             </div>
             <div class="mb-3">
                 <label for="writers" class="form-label">Writers (Separare i valori tra le virgole)</label>
                 <input type="text" class="form-control" name="writers" id="writers" aria-describedby="helpwriters"
-                    placeholder="Insert a comic writers">
+                    placeholder="Insert a comic writers" value="{{ implode(', ', $comic->writers) }}">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
